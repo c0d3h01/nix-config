916da136b6f2aa2a2c45f5d11721f1163d74ee9d
c0d3h01 <c0d3h01@gmail.com>
2025-03-10 14:07:07 +0530

    chore: settings, CSS

--- home/modules/firefox.nix
+++ home/modules/firefox.nix
@@ -10,77 +10,101 @@ in
     programs.firefox = {
       enable = true;
 
-      # Privacy about:config settings
+      # Balance privacy and functionality
       profiles.c0d3h01 = {
-
-        # Install extensions from NUR
+        # Essential extensions
         extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
+          # privacy-badger  
+          # darkreader
+          # bitwarden # password manager
         ];
 
         settings = {
+          # Enable screenshot functionality
+          "extensions.screenshots.disabled" = false;
+          "screenshots.browser.component.enabled" = true;
+          
+          # Enable context menu for screenshots
+          "screenshots.browser.action.disabled" = false;
+          
+          # Keyboard shortcut
+          "devtools.screenshot.clipboard.enabled" = true;
+          "devtools.screenshot.audio.enabled" = true;
+          
+          # Basic privacy settings - less aggressive
           "browser.send_pings" = false;
-          "browser.urlbar.speculativeConnect.enabled" = false;
+          "browser.urlbar.speculativeConnect.enabled" = true;
           "dom.event.clipboardevents.enabled" = true;
-          "media.navigator.enabled" = false;
-          "network.cookie.cookieBehavior" = 1;
-          "network.http.referer.XOriginPolicy" = 2;
+          "media.navigator.enabled" = true;
+          "network.cookie.cookieBehavior" = 3;
+          "network.http.referer.XOriginPolicy" = 1;
           "network.http.referer.XOriginTrimmingPolicy" = 2;
-          "beacon.enabled" = false;
-          "browser.safebrowsing.downloads.remote.enabled" = false;
+          "beacon.enabled" = true;
+          "browser.safebrowsing.downloads.remote.enabled" = true;
           "network.IDN_show_punycode" = true;
           "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
           "app.shield.optoutstudies.enabled" = false;
           "dom.security.https_only_mode_ever_enabled" = true;
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          "browser.toolbars.bookmarks.visibility" = "never";
+          "browser.toolbars.bookmarks.visibility" = "always";
           "geo.enabled" = false;
 
-          # Disable telemetry
+          # Balanced telemetry approach
           "browser.newtabpage.activity-stream.feeds.telemetry" = false;
           "browser.ping-centre.telemetry" = false;
-          "browser.tabs.crashReporting.sendReport" = false;
+          "browser.tabs.crashReporting.sendReport" = true;
           "devtools.onboarding.telemetry.logged" = false;
           "toolkit.telemetry.enabled" = false;
           "toolkit.telemetry.unified" = false;
           "toolkit.telemetry.server" = "";
 
-          # Disable Pocket
+          # Pocket settings (less restrictive)
           "browser.newtabpage.activity-stream.feeds.discoverystreamfeed" = false;
           "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
           "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
           "browser.newtabpage.activity-stream.showSponsored" = false;
           "extensions.pocket.enabled" = false;
 
-          # Disable prefetching
-          "network.dns.disablePrefetch" = true;
-          "network.prefetch-next" = false;
+          # Enable prefetching for better performance
+          "network.dns.disablePrefetch" = false;
+          "network.prefetch-next" = true;
 
-          # Disable JS in PDFs
-          "pdfjs.enableScripting" = false;
+          # PDF settings
+          "pdfjs.enableScripting" = true;
 
-          # Harden SSL 
+          # SSL settings
           "security.ssl.require_safe_negotiation" = true;
 
-          # Extra
+          # Usability improvements
           "identity.fxaccounts.enabled" = true;
           "browser.search.suggest.enabled" = true;
           "browser.urlbar.shortcuts.bookmarks" = true;
           "browser.urlbar.suggest.bookmark" = true;
-          "browser.urlbar.shortcuts.history" = false;
-          "browser.urlbar.shortcuts.tabs" = false;
-          "browser.urlbar.suggest.engines" = false;
-          "browser.urlbar.suggest.history" = false;
-          "browser.urlbar.suggest.openpage" = false;
-          "browser.urlbar.suggest.topsites" = false;
+          "browser.urlbar.shortcuts.history" = true;
+          "browser.urlbar.shortcuts.tabs" = true;
+          "browser.urlbar.suggest.engines" = true; 
+          "browser.urlbar.suggest.history" = true;
+          "browser.urlbar.suggest.openpage" = true;
+          "browser.urlbar.suggest.topsites" = true;
           "browser.uidensity" = 1;
           "media.autoplay.enabled" = false;
-          "toolkit.zoomManager.zoomValues" = ".8,.90,.95,1,1.1,1.2";
+          "toolkit.zoomManager.zoomValues" = ".8,.90,.95,1,1.1,1.2,1.5,2.0";
           "privacy.firstparty.isolate" = false;
-          "network.http.sendRefererHeader" = 0;
+          "network.http.sendRefererHeader" = 2;
+          
+          # Performance improvements
+          "browser.cache.disk.enable" = true;
+          "browser.cache.memory.enable" = true;
+          "browser.cache.memory.capacity" = 524288;
+          "browser.sessionstore.interval" = 60000;
+          
+          # Media and DRM (needed for streaming services)
+          "media.eme.enabled" = true;
+          "media.gmp-widevinecdm.enabled" = true;
         };
 
-        # userChome.css to make it look better
+        # userChome.css remains the same
         userChrome = "
                     * { 
                         box-shadow: none !important;
@@ -92,10 +116,11 @@ in
                     }
 
                     .tab-background {
-                        border-radius: var(--user-tab-rounding) var(--user-tab-rounding) 0px 0px !important; /* Connected */
-                        margin-block: 1px 0 !important; /* Connected */
+                        border-radius: var(--user-tab-rounding) var(--user-tab-rounding) 0px 0px !important;
+                        margin-block: 1px 0 !important;
                     }
-                    #scrollbutton-up, #scrollbutton-down { /* 6/10/2021 */
+                    
+                    #scrollbutton-up, #scrollbutton-down {
                         border-top-width: 1px !important;
                         border-bottom-width: 0 !important;
                     }
@@ -104,6 +129,7 @@ in
                         --lwt-tabs-border-color: rgba(0, 0, 0, 0.5) !important;
                         border-bottom-color: transparent !important;
                     }
+                    
                     [brighttext='true'] .tab-background:is([selected], [multiselected]):-moz-lwtheme {
                         --lwt-tabs-border-color: rgba(255, 255, 255, 0.5) !important;
                         border-bottom-color: transparent !important;
@@ -117,10 +143,12 @@ in
                     #TabsToolbar, #tabbrowser-tabs {
                         --tab-min-height: 29px !important;
                     }
+                    
                     #main-window[sizemode='true'] #toolbar-menubar[autohide='true'] + #TabsToolbar, 
                     #main-window[sizemode='true'] #toolbar-menubar[autohide='true'] + #TabsToolbar #tabbrowser-tabs {
                         --tab-min-height: 30px !important;
                     }
+                    
                     #scrollbutton-up,
                     #scrollbutton-down {
                         border-top-width: 0 !important;
@@ -130,6 +158,7 @@ in
                     #TabsToolbar, #TabsToolbar > hbox, #TabsToolbar-customization-target, #tabbrowser-arrowscrollbox  {
                         max-height: calc(var(--tab-min-height) + 1px) !important;
                     }
+                    
                     #TabsToolbar-customization-target toolbarbutton > .toolbarbutton-icon, 
                     #TabsToolbar-customization-target .toolbarbutton-text, 
                     #TabsToolbar-customization-target .toolbarbutton-badge-stack,
