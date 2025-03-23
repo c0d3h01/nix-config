f4cb1c6052bc22b87fd2b9b76a2616486b297571
c0d3h01 <c0d3h01@gmail.com>
2025-03-23 12:45:05 +0530

    feat: restructure printing service configuration for improved clarity

--- nix/apps/printing.nix
+++ nix/apps/printing.nix
@@ -1,9 +1,11 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   # -*- Enable printing -*-
-  services.printing.enable = true;
-  services.avahi.enable = true;
-  services.avahi.openFirewall = true;
+  services = {
+    printing.enable = true;
+    avahi.enable = true;
+    avahi.openFirewall = true;
+  };
   networking.firewall.allowedTCPPorts = [ 631 ];
 }
