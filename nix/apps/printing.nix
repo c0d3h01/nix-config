b24439fff7bf47273e9c72fdfa2d760cda9b67e0
c0d3h01 <c0d3h01@gmail.com>
2025-03-23 19:40:57 +0530

    upd: enable printing services and add drivers in Nix configuration

--- nix/apps/printing.nix
+++ nix/apps/printing.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   # -*- Enable printing -*-
   services = {
     printing.enable = true;
+    printing.drivers = with pkgs; [ gutenprint hplip ];
     avahi.enable = true;
     avahi.openFirewall = true;
   };
