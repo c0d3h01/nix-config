652ff764117b93957122cb5fba15ab720f2a0356
c0d3h01 <c0d3h01@gmail.com>
2025-04-08 12:08:09 +0530

    upd: define SSH key configuration in secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,15 @@
 let
-  keys = import ../keys.nix;
+  keys = import ./keys.nix;
 in
 with keys;
-{ }
+{
+  # SSH Key
+  "ssh-private-key" = {
+    file = ./ssh-private-key.age;
+    owner = "c0d3h01";
+    group = "users";
+    mode = "600";
+    path = "/home/c0d3h01/.ssh/id_ed25519";
+    publicKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPMHEDTYxp5o3ZLpEg4fWjXZ/MctVco8R9qYVfE0tIn+ c0d3h01@gmail.com" ];
+  };
+}
