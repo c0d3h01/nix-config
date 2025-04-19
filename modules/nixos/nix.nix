8b6223f2f1cbf15a774e5183b122a3f7bacc1f8e
c0d3h01 <haarshalsawant@gmail.com>
2025-04-19 13:58:57 +0530

    refactor(nix.nix): improve formatting and remove unused Cachix key

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -5,9 +5,11 @@
   userConfig,
   lib,
   ...
-}: let
+}:
+let
   flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-in {
+in
+{
   nixpkgs = {
     overlays = builtins.attrValues outputs.overlays;
     config = {
@@ -30,7 +32,7 @@ in {
   };
 
   nix = {
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
 
     settings = {
       flake-registry = "";
@@ -55,13 +57,11 @@ in {
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
-        "https://hyprland.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
