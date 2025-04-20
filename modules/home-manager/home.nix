fdb57719c474d0a2d0b589a03c46e52aa173bfe6
c0d3h01 <haarshalsawant@gmail.com>
2025-04-20 12:15:20 +0530

    rmoved unused modules, configs

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -29,7 +29,7 @@
       EDITOR = "nvim";
       VISUAL = "nvim";
       TERMINAL = "kitty";
-      BROWSER = "firefox";
+      BROWSER = "brave";
       PAGER = "less";
       LESS = "-R";
       JAVA_HOME = "${pkgs.openjdk}/lib/openjdk";
@@ -37,7 +37,7 @@
       # ANDROID_SDK_ROOT = "/home/c0d3h01/Android";
       # ANDROID_NDK_HOME = "/home/c0d3h01/Android/android-ndk-r27c";
       # PATH = "$HOME/Android/cmdline-tools/bin:$HOME/Android/platform-tools:$HOME/Android/android-ndk-r27c:$PATH";
-      CHROME_EXECUTABLE = "${pkgs.firefox}/bin/firefox";
+      CHROME_EXECUTABLE = "${pkgs.brave}/bin/brave";
     };
 
     sessionPath = [
--- modules/nixos/desktop/hyprwm/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Start background services
-gnome-keyring-daemon --start --components=secrets &
-hyprpaper &
-nm-applet --indicator &
-ags &
-hyprpanel &
-dunst &
