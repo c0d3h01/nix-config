eb9b8948294b413602070767cf75947b86dcc714
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-05-20 18:20:04 +0530

    fix(tweaks):  powertop pkg

--- nixos/tweaks.nix
+++ nixos/tweaks.nix
@@ -1,9 +1,13 @@
-{ config, lib, ... }:
+{ pkgs, ... }:
 
 {
   services.power-profiles-daemon.enable = false;
   powerManagement.powertop.enable = true;
-  services.thermald.enable = true;
+  # services.thermald.enable = true;
   # powerManagement.cpuFreqGovernor = "ondemand";
   services.auto-cpufreq.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    powertop
+  ];
 }
