2e3f03523d9dc31e21aa057e017f8da4739644e6
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-05-20 15:25:32 +0530

    fix: update user group  in libvirt configuration

--- nixos/libvirt.nix
+++ nixos/libvirt.nix
@@ -1,6 +1,6 @@
 {
   virtualisation.libvirtd.enable = true;
-  users.users.joerg.extraGroups = [ "libvirtd" ];
+  users.users.c0d3h01.extraGroups = [ "libvirtd" ];
   networking.firewall.checkReversePath = false;
   programs.virt-manager.enable = true;
 }
