3c8c5c7bcddcb1d34c692907fe4f53bc0b76116a
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-05-26 04:43:17 +0000

    chore: changed filesystem f2fs into ext4

--- machines/installer/disko-config.nix
+++ machines/installer/disko-config.nix
@@ -9,8 +9,8 @@
           partitions = {
             ESP = {
               name = "nixos-esp";
-              end = "512M";
               type = "EF00";
+              size = "512M";
               content = {
                 type = "filesystem";
                 format = "vfat";
@@ -18,29 +18,13 @@
                 mountOptions = [ "umask=0077" ];
               };
             };
-            plainSwap = {
-              name = "nixos-swap";
-              size = "4G";
-              content = {
-                type = "swap";
-                discardPolicy = "both";
-                resumeDevice = true;
-              };
-            };
             root = {
-              name = "root";
-              end = "-0";
+              name = "nixos-root";
+              size = "100%";
               content = {
                 type = "filesystem";
-                format = "f2fs";
+                format = "ext4";
                 mountpoint = "/";
-                extraArgs = [
-                  "-O"
-                  "extra_attr,inode_checksum,sb_checksum,compression"
-                ];
-                mountOptions = [
-                  "compress_algorithm=zstd:6,compress_chksum,atgc,gc_merge,lazytime,nodiscard"
-                ];
               };
             };
           };
