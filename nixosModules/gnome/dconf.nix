19051ebf48cf922d7c49cf7a88ed2bc3dfd8d364
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-10 13:52:11 +0530

    feat(dconf.nix): add user-specific GNOME settings and configurations

---
+++ nixosModules/gnome/dconf.nix
@@ -0,0 +1,67 @@
+{ declarative, pkgs, ... }:
+
+{
+  home-manager.users."${declarative.username}" = {
+    dconf.settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = [
+          "gsconnect@andyholmes.github.io"
+          "dash-to-dock@micxgx.gmail.com"
+        ];
+      };
+
+      "org/gnome/settings-daemon/plugins/power" = {
+        power-button-action = "Power Off";
+        sleep-inactive-ac-type = "Power Off";
+      };
+
+      # Dask to Dock
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        dock-position = "BOTTOM";
+      };
+
+      # interface
+      "org/gnome/desktop/interface" = {
+        enable-hot-corners = false;
+        clock-show-weekday = true;
+        clock-show-date = true;
+        clock-format = "12h";
+        enable-animations = true;
+      };
+
+      # touchpad
+      "org/gnome/desktop/peripherals/touchpad" = {
+        tap-to-click = true;
+        two-finger-scrolling-enabled = true;
+        natural-scroll = true;
+        disable-while-typing = true;
+      };
+
+      # keyboard
+      "org/gnome/desktop/peripherals/keyboard" = {
+        numlock-state = true;
+      };
+
+      # workspaces
+      "org/gnome/mutter" = {
+        dynamic-workspaces = true;
+        workspaces-only-on-primary = true;
+      };
+
+      # wallpaper
+      # "org/gnome/desktop/background" = {
+      #   picture-uri = "file:///home/${declarative.username}/dotfiles/assets/wallpapers/Space-Nebula.png";
+      #   picture-uri-dark = "file:///home/${declarative.username}/dotfiles/assets/wallpapers/Space-Nebula.png";
+      #   picture-options = "zoom";
+      # };
+    };
+
+    # Configure XDG portals for the user
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [ xdg-desktop-portal-gnome ];
+      config.common.default = "*";
+    };
+  };
+}
--- nixosModules/gnome/default.nix
+++ nixosModules/gnome/default.nix
@@ -5,10 +5,15 @@
 }:
 
 {
+  imports = [
+    ./dconf.nix
+  ];
+
   # Enable Gnome, X server
   services.desktopManager.gnome.enable = true;
   services.displayManager.gdm.enable = true;
   services.xserver.desktopManager.xterm.enable = false;
+  services.gnome.gnome-initial-setup.enable = false;
 
   programs.kdeconnect = {
     enable = true;
@@ -32,6 +37,7 @@
       gnome-maps
       gnome-connections
       gnome-system-monitor
+      gnome-user-docs
     ];
   };
 
@@ -39,79 +45,11 @@
     with pkgs.gnomeExtensions;
     [
       dash-to-dock
-      # user-themes
-      # blur-my-shell
-      open-bar
     ]
     ++ (with pkgs; [
       gnome-photos
       gnome-tweaks
+      gradience
+      gnome-chess
     ]);
-
-  home-manager.users."${declarative.username}" = {
-    dconf.settings = {
-      "org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = [
-          "gsconnect@andyholmes.github.io"
-          "openbar@neuromorph"
-          "dash-to-dock@micxgx.gmail.com"
-          # "user-theme@gnome-shell-extensions.gcampax.github.com"
-          # "blur-my-shell@aunetx"
-        ];
-      };
-
-      # # User themes
-      # "org/gnome/shell/extensions/user-theme" = {
-      #   name = "WhiteSur-Dark";
-      # };
-
-      # Dask to Dock
-      "org/gnome/shell/extensions/dash-to-dock" = {
-        dock-position = "BOTTOM";
-        intellihide-mode = "ALL-WINDOWS";
-      };
-
-      # interface
-      "org/gnome/desktop/interface" = {
-        enable-hot-corners = true;
-        clock-show-weekday = true;
-        clock-show-date = true;
-        clock-format = "12h";
-        color-scheme = "prefer-dark";
-      };
-
-      # touchpad
-      "org/gnome/desktop/peripherals/touchpad" = {
-        tap-to-click = true;
-        two-finger-scrolling-enabled = true;
-        natural-scroll = true;
-      };
-
-      # keyboard
-      "org/gnome/desktop/peripherals/keyboard" = {
-        numlock-state = true;
-      };
-
-      # workspaces
-      "org/gnome/mutter" = {
-        dynamic-workspaces = true;
-        workspaces-only-on-primary = true;
-      };
-
-      # wallpaper
-      # "org/gnome/desktop/background" = {
-      #   picture-uri = "file:///home/${declarative.username}/dotfiles/assets/wallpapers/Space-Nebula.png";
-      #   picture-uri-dark = "file:///home/${declarative.username}/dotfiles/assets/wallpapers/Space-Nebula.png";
-      #   picture-options = "zoom";
-      # };
-    };
-
-    # Configure XDG portals for the user
-    xdg.portal = {
-      enable = true;
-      extraPortals = with pkgs; [ xdg-desktop-portal-gnome ];
-      config.common.default = "*";
-    };
-  };
 }
