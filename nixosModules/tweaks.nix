5dfad974f6ab3cb3323a4b3e31069058d45861ee
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-20 21:41:30 +0530

    feat(tweaks): Power management, TLP settings

--- nixosModules/tweaks.nix
+++ nixosModules/tweaks.nix
@@ -1,17 +1,26 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  powerManagement.powertop.enable = true;
-  systemd.services.powertop.postStart = ''
-    HIDDEVICES=$(ls /sys/bus/usb/drivers/usbhid | grep -oE '^[0-9]+-[0-9\.]+' | sort -u)
-    for i in $HIDDEVICES; do
-      echo -n "Enabling " | cat - /sys/bus/usb/devices/$i/product
-      echo 'on' > /sys/bus/usb/devices/$i/power/control
-    done
-  '';
-
-  systemd.services.tlp = lib.mkIf (config.services.tlp.enable) { after = [ "powertop.service" ]; };
-
-  # powerManagement.cpuFreqGovernor = lib.mkDefault "schedutil";
   systemd.oomd.enable = true;
+  services.acpid.enable = true;
+
+  services.ananicy = {
+    enable = true;
+    package = pkgs.ananicy-cpp
+  };
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      USB_AUTOSUSPEND = 1;
+      CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
+
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+    # cpuFreqGovernor = "schedutil";
+  };
 }
