b238cd515f6bbf5c800e3b6ef2c30f800f0f2a8a
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-25 12:03:07 +0530

    refactor(overlays): remove unused hello-overlay and default.nix files

--- overlays/default.nix
+++
@@ -1,3 +0,0 @@
-[
-  (import ./hello-overlay.nix)
-]
--- overlays/hello-overlay.nix
+++
@@ -1,7 +0,0 @@
-final: prev: {
-  hello = prev.hello.overrideAttrs (old: {
-    meta = old.meta // {
-      description = "Hello, but with a custom overlay!";
-    };
-  });
-}
