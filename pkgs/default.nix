2bdd8b80724e21c49156241d0fbb69e7cbdc7195
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-27 09:28:37 +0530

    refactor(devModules): shift modules to apps

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,7 +10,7 @@
 
   # <-- Enable flatpak repo --> :
   # flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-  services.flatpak.enable = true;
+  # services.flatpak.enable = true;
 
   # AppImage support
   programs.appimage = {
@@ -18,9 +18,31 @@
     binfmt = true;
   };
 
+    # <-- Coustom modules -->
+  myModules = {
+    # docker.enable = true;
+    go.enable = true;
+    java.enable = true;
+    # monitoring.enable = true;
+    mysql.enable = true;
+    node.enable = true;
+    podman.enable = true;
+    python.enable = true;
+    r.enable = true;
+    rust.enable = true;
+  };
+
+  programs.wireshark = {
+    enable = true;
+    package = pkgs.wireshark;
+    dumpcap.enable = true;
+    usbmon.enable = true;
+  };
+
   # <-- Environment packages -->
   environment.systemPackages = with pkgs; [
-    # <-- Desktop applications -->
+
+    # Browsers
     firefox
 
     # Notion Enhancer With patches
@@ -54,5 +76,23 @@
     obs-studio
     libreoffice-qt6-fresh
     obsidian
+
+    # Terminal tools
+    electron
+    toolbox
+    umlet
+    gdb
+    gcc
+    gnumake
+    cmake
+    ninja
+    clang
+    pkg-config
+    gtk4
+    glib
+    pango
+    gdk-pixbuf
+    gobject-introspection
+    libepoxy
   ];
 }
--- pkgs/devModules/default.nix
+++ pkgs/devModules/default.nix
@@ -1,9 +1,4 @@
 {
-  pkgs,
-  ...
-}:
-
-{
   imports = [
     ./docker.nix
     ./go.nix
@@ -15,45 +10,5 @@
     ./python.nix
     ./r.nix
     ./rust.nix
-  ];
-
-  # <-- Coustom modules -->
-  myModules = {
-    # docker.enable = true;
-    go.enable = true;
-    java.enable = true;
-    # monitoring.enable = true;
-    mysql.enable = true;
-    node.enable = true;
-    podman.enable = true;
-    python.enable = true;
-    r.enable = true;
-    rust.enable = true;
-  };
-
-  programs.wireshark = {
-    enable = true;
-    package = pkgs.wireshark;
-    dumpcap.enable = true;
-    usbmon.enable = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    electron
-    toolbox
-    umlet
-    gdb
-    gcc
-    gnumake
-    cmake
-    ninja
-    clang
-    pkg-config
-    gtk4
-    glib
-    pango
-    gdk-pixbuf
-    gobject-introspection
-    libepoxy
   ];
 }
