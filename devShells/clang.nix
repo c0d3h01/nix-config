f2bbccc12e4564332c5cf3a9b9799e3463e3ca21
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-29 14:15:10 +0530

    feat: dev shells- clang, gtk

---
+++ devShells/clang.nix
@@ -0,0 +1,19 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  name = "Clang Dev Shell";
+  buildInputs = with pkgs; [
+      gdb
+      gcc
+      gnumake
+      cmake
+      cmakeWithGui
+      ninja
+      clang
+      pkg-config
+  ];
+  # shellHook = ''
+  # '';
+}
---
+++ devShells/gtk.nix
@@ -0,0 +1,15 @@
+{
+    pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+    name = "GTK Shell";
+    buildInputs = with pkgs; [
+      gtk4
+      glib
+      pango
+      gdk-pixbuf
+      gobject-introspection
+      libepoxy
+    ];
+}
