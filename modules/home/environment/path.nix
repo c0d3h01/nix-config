9a4be90f0df3d00de11d675f26bb713a824c81d4
c0d3h01 <harshalsawant.dev@gmail.com>
2025-07-18 20:30:12 +0530

    chore: update home.sessionPath with additional directories

--- modules/home/environment/path.nix
+++ modules/home/environment/path.nix
@@ -10,6 +10,12 @@ in
 {
   home.sessionPath = [
     "${config.home.homeDirectory}/.local/bin"
+    "${config.home.homeDirectory}/.npm-global/bin"
+    "${config.home.homeDirectory}/.rustup"
+    "${config.home.homeDirectory}/.cargo/bin"
+    "${config.home.homeDirectory}/.cargo/env"
+    "${config.home.homeDirectory}/go/bin"
+    "${config.home.homeDirectory}/bin"
     # I relocated this too the fish config, such that it would fix a issue where git would use the wrong version
     # "/etc/profiles/per-user/c0d3h01/bin" # needed for darwin
   ] ++ optional pkgs.stdenv.hostPlatform.isDarwin "$GHOSTTY_BIN_DIR";
