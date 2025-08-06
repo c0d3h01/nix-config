47b61423334ab010f36b6f7b12e435709586230e
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-06 12:55:31 +0530

    refactor: enhance wireless configuration with additional comments and structure

--- nixosConfigs/modules/networking/wireless.nix
+++ nixosConfigs/modules/networking/wireless.nix
@@ -1,16 +1,30 @@
 {
+  lib,
   userConfig,
   ...
 }:
 {
+
+  # Ensures Wi-Fi adheres to your country's power/channel rules
   hardware.wirelessRegulatoryDatabase = true;
+
+  # === GUI Tool for Managing Secrets (WiFi passwords, SSH keys) ===
+  programs.seahorse.enable = true;
+
+  # Enable MAC randomization during scanning (good for privacy)
+  networking.networkmanager.wifi.scanRandMacAddress = true;
 
   networking.wireless = {
     # wpa_supplicant
     enable = userConfig.machineConfig.networking.backend == "wpa_supplicant";
+
+    # Allow user to manage networks via `nmcli` or GUI
     userControlled.enable = true;
+
+    # Allow imperative commands like `iwlist scan`, `nmcli dev wifi connect`
     allowAuxiliaryImperativeNetworks = true;
 
+    # wpa_supplicant: Save network config in /etc/NetworkManager/system-connections/
     extraConfig = ''
       update_config=1
     '';
@@ -26,6 +40,8 @@
           # more things that my uni hates me for
           # AddressRandomization = "network";
           # AddressRandomizationRange = "full";
+
+          # Enable dynamic IP + IPv6 router-based config
           EnableNetworkConfiguration = true;
           RoamRetryInterval = 15;
         };
