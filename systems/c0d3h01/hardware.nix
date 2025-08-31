b5a5fa60be941b73e3e2476c2ed8078cca2bffe5
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-31 09:20:00 +0530

    shift partitionin from ext4 -> btrfs with compressions & subvolumes

--- systems/c0d3h01/hardware.nix
+++ systems/c0d3h01/hardware.nix
@@ -24,7 +24,7 @@
 
             plainSwap = {
               label = "nixos-swap";
-              size = "12G";
+              size = "8G";
               content = {
                 type = "swap";
                 discardPolicy = "both";
@@ -36,13 +36,77 @@
               label = "nixos-root";
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-                mountOptions = [
-                  "defaults"
-                  "noatime"
-                ];
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/@" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "noatime"
+                      "compress=zstd:1"
+                      "ssd"
+                      "commit=120"
+                    ];
+                  };
+
+                  "/@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [
+                      "noatime"
+                      "compress=zstd:1"
+                      "ssd"
+                      "commit=120"
+                    ];
+                  };
+
+                  "/@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "noatime"
+                      "compress=zstd:3"
+                      "ssd"
+                      "commit=120"
+                    ];
+                  };
+
+                  "/@srv" = {
+                    mountpoint = "/srv";
+                    mountOptions = [
+                      "noatime"
+                      "compress=zstd:1"
+                      "ssd"
+                      "commit=120"
+                    ];
+                  };
+
+                  "/@cache" = {
+                    mountpoint = "/var/cache";
+                    mountOptions = [
+                      "nodatacow"
+                      "nodatasum"
+                      "ssd"
+                      "commit=120"
+                    ];
+                  };
+
+                  "/@tmp" = {
+                    mountpoint = "/var/tmp";
+                    mountOptions = [
+                      "nodatacow"
+                      "ssd"
+                      "commit=120"
+                    ];
+                  };
+
+                  "/@log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = [
+                      "nodatacow"
+                      "ssd"
+                      "commit=120"
+                    ];
+                  };
+                };
               };
             };
           };
