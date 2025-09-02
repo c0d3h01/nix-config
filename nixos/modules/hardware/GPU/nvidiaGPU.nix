8e681a7993d4acaebb38e2cc03ea79e319c92339
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-09-02 18:02:18 +0530

    refactor: enhance NVIDIA GPU configuration by removing redundant packages and enabling power management

--- nixos/modules/hardware/GPU/nvidiaGPU.nix
+++ nixos/modules/hardware/GPU/nvidiaGPU.nix
@@ -12,33 +12,33 @@ let
 in
 {
   config = mkIf (cfg == "nvidia") {
+
+    # xorg drivers
+    services.xserver.videoDrivers = [ "nvidia" ];
+
     hardware.graphics = lib.mkIf isWorskstaion {
       enable = true;
 
       extraPackages = with pkgs; [
-        libva
-        libvdpau
-        vulkan-loader
         nvidia-vaapi-driver
         nvidia-vulkan-vaapi
       ];
 
       enable32Bit = true;
       extraPackages32 = with pkgs.pkgsi686Linux; [
-        libva
-        libvdpau
-        vulkan-loader
+        nvidia-vaapi-driver
+        nvidia-vulkan-vaapi
       ];
     };
 
     # NVIDIA driver configuration
     hardware.nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.stable;
-      modesetting.enable = true;
-      powerManagement.enable = false;
+      powerManagement.enable = true;
       powerManagement.finegrained = false;
       open = lib.mkDefault false;
       nvidiaSettings = true;
+      nvidiaPersistenced = true;
       dynamicBoost.enable = true;
     };
 
@@ -46,6 +46,7 @@ in
     environment.systemPackages = with pkgs; [
       glxinfo
       vulkan-tools
+      vulkan-loader
       libva-utils
       vdpauinfo
     ];
@@ -61,12 +62,6 @@ in
     environment.sessionVariables = {
       LIBVA_DRIVER_NAME = "nvidia";
       VDPAU_DRIVER = "nvidia";
-    }
-    // lib.optionalAttrs config.hardware.nvidia.prime.offload.enable {
-      __NV_PRIME_RENDER_OFFLOAD = "1";
-      __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      __VK_LAYER_NV_optimus = "NVIDIA_only";
     };
   };
 }
