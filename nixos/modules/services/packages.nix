62736af7adf8866379578c7abc2cb60b25c5d546
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-12-30 09:47:11 +0530

    fix: remove unnecessary system utilities from configuration

--- nixos/modules/services/packages.nix
+++ nixos/modules/services/packages.nix
@@ -36,26 +36,11 @@
     ouch
     colordiff
     openssl
-    lsof
     inxi
-    strace
-    ltrace
     rsync
-    binutils
-    usbutils
-    pciutils
     iperf
     wget
     curl
-    iw
-    tcpdump
-    netcat
-    whois
-    dnsutils
-    tcpflow
-    tcpreplay
-    netcat-gnu
-    netmask
   ];
 in {
   environment.systemPackages = (optionals isWorkstation desktopApps) ++ devSystemTools;
